c1ce4d6d4983326cc07a9c5df0bb953caba12194
soo4767 <soo4767@naver.com>
2021-08-27 12:45:03 +0900

    upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const mongoose = require('mongoose')
+
+
+mongoose.connect('mongodb+srv://minsoo:s2331747@boilerplate.g9qr2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser : true, useUnifiedTopology : true , useCreateIndex : true , useFindAndModify : false
+}).then(()=> console.log("mongoDB Connected..")) 
+.catch(err => console.log(err))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
